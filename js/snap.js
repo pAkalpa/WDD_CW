1529313670dddc17f9da24a4f2e051c5415ad539
pasindu <pasinduakalpa1998@gmail.com>
2021-07-09 01:22:04 +0530

    :lipstick: Many Minor UI Improvements

--- js/snap.js
+++ js/snap.js
@@ -1,15 +1,20 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll(".panel");
 
-panels.forEach(panel => {
-    panel.addEventListener('click',() => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+panels.forEach((panel) => {
+  panel.addEventListener("mouseover", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    console.log(panel.className);
+  });
+});
 
 function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
 }
-
